2f0780c56817fa3305e75b948fb314a5b6c92e04
Einar Nilsen-Nygaard <einarnn@gmail.com>
2018-11-29 13:13:17 +0000

    Simple blocking lock client-side implementation

--- ncclient/operations/lock.py
+++ ncclient/operations/lock.py
@@ -15,8 +15,11 @@
 "Locking-related NETCONF operations"
 
 from ncclient.xml_ import *
-
 from ncclient.operations.rpc import RaiseMode, RPC
+import time
+
+# if a blocking lock is requested, how long between lock attempts?
+DEFAULT_BLOCKING_WAIT= 0.01
 
 # TODO: parse session-id from a lock-denied error, and raise a tailored exception?
 
@@ -25,14 +28,60 @@ class Lock(RPC):
     "`lock` RPC"
 
 
-    def request(self, target="candidate"):
+    def request(self, target="candidate", blocking=False, retries=0, interval=DEFAULT_BLOCKING_WAIT):
         """Allows the client to lock the configuration system of a device.
 
         *target* is the name of the configuration datastore to lock
         """
         node = new_ele("lock")
         sub_ele(sub_ele(node, "target"), target)
-        return self._request(node)
+        if not blocking:
+            return self._request(node)
+
+        # fall through to blocking behavior
+        self.logger.debug('Using blocking lock behavior')
+        raise_mode_original = self._raise_mode
+        tries = 0
+        self._raise_mode = RaiseMode.NONE
+        while True:
+
+            # do the lock, and if it works just return the result
+            lock_result = self._request(node)
+            if lock_result.ok:
+                break
+
+            # check if we need to keep going
+            tries += 1
+            if retries > 0 and tries > retries:
+                break
+
+            # if we get an error apart from 'lock-denied', we need to
+            # break out
+            if lock_result.errors[0].tag != 'lock-denied':
+                break
+            
+            # if we get this far, we sleep for the interval specified,
+            # re-prime listener, using the same message id, and go
+            # round again
+            time.sleep(interval)
+            self._listener.register(self._id, self)
+
+            # log a message periodically if we're trying for a long time...
+            if not (tries % 10):
+                self.logger.debug('Attempted to acquire lock %d times so far', tries)
+
+        # we might need to raise an exception here...
+        if not lock_result.ok:
+            if raise_mode_original == RaiseMode.ALL or (raise_mode_original == RaiseMode.ERRORS and lock_result.error.severity == "error"):
+                errors = self._reply.errors
+                if len(errors) > 1:
+                    raise RPCError(to_ele(self._reply._raw), errs=errors)
+                else:
+                    raise self._reply.error
+
+        # return whatever the last result was
+        return lock_result
+            
 
 
 class Unlock(RPC):
